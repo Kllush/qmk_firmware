6a187cea593e5ce7a693e139412869c673bd1df3
bryan065 <le.bryan.065@gmail.com>
2021-05-12 18:58:00 -0400

    update

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/cyber77/config.h
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/cyber77/config.h
@@ -2,10 +2,13 @@
 #undef RGB_DI_PIN
 #undef BACKLIGHT_PIN
 
-//#define B7_AUDIO
 #define DYNAMIC_MACRO_NO_NESTING
 #define PERMISSIVE_HOLD_PER_KEY
 #define TAPPING_TERM_PER_KEY
+
+// Free up space
+#define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
 
 #ifdef ENCODER_ENABLE
     #define ENCODERS_PAD_A { B3 } //MISO
@@ -13,6 +16,8 @@
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
+    #undef RGBLIGHT_ENABLE
+
     //RGB Matrix Effects
     #define RGB_MATRIX_KEYPRESSES                                // Keypress effects
     #define RGB_MATRIX_FRAMEBUFFER_EFFECTS                       // Frame buffer effects
@@ -48,45 +53,60 @@
     #define RGB_MATRIX_STARTUP_SPD 64
 #endif
 
-// Free up space
-#define NO_ACTION_ONESHOT
-#define NO_MUSIC_MODE
+#ifdef RGBLIGHT_ENABLE
+    #undef RGB_MATRIX_ENABLE
 
-#undef IMPERIAL_MARCH
-#define IMPERIAL_MARCH \
-  HD_NOTE(_A4), HD_NOTE(_A4), HD_NOTE(_A4), QD_NOTE(_F4), QD_NOTE(_C5), \
-  HD_NOTE(_A4), QD_NOTE(_F4),  QD_NOTE(_C5), WD_NOTE(_A4), \
-  HD_NOTE(_E5), HD_NOTE(_E5), HD_NOTE(_E5), QD_NOTE(_F5), QD_NOTE(_C5), \
-  HD_NOTE(_A4), QD_NOTE(_F4),  QD_NOTE(_C5), WD_NOTE(_A4)
+    #define RGB_DI_PIN B2
+    #define RGBLED_NUM 10
 
-#undef RICK_ROLL
-#define RICK_ROLL      \
-    Q__NOTE(_F4),      \
-    Q__NOTE(_G4),      \
-    Q__NOTE(_BF4),     \
-    Q__NOTE(_G4),      \
-    HD_NOTE(_D5),      \
-    HD_NOTE(_D5),      \
-    W__NOTE(_C5),      \
-    S__NOTE(_REST),    \
-    Q__NOTE(_F4),      \
-    Q__NOTE(_G4),      \
-    Q__NOTE(_BF4),     \
-    Q__NOTE(_G4),      \
-    HD_NOTE(_C5),      \
-    HD_NOTE(_C5),      \
-    W__NOTE(_BF4),     \
-    S__NOTE(_REST),    \
-    Q__NOTE(_F4),      \
-    Q__NOTE(_G4),      \
-    Q__NOTE(_BF4),     \
-    Q__NOTE(_G4),      \
-    W__NOTE(_BF4),     \
-    H__NOTE(_C5),      \
-    H__NOTE(_A4),      \
-    H__NOTE(_A4),      \
-    H__NOTE(_G4),      \
-    H__NOTE(_F4),      \
-    H__NOTE(_F4),      \
-    W__NOTE(_C5),      \
-    W__NOTE(_BF4),
+    //#define RGBLIGHT_ANIMATIONS
+    #define RGBLIGHT_EFFECT_ALTERNATING
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+
+    #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+#endif
+
+#ifdef AUDIO_ENABLE
+    #define B7_AUDIO
+
+    #undef IMPERIAL_MARCH
+    #define IMPERIAL_MARCH \
+    HD_NOTE(_A4), HD_NOTE(_A4), HD_NOTE(_A4), QD_NOTE(_F4), QD_NOTE(_C5), \
+    HD_NOTE(_A4), QD_NOTE(_F4),  QD_NOTE(_C5), WD_NOTE(_A4), \
+    HD_NOTE(_E5), HD_NOTE(_E5), HD_NOTE(_E5), QD_NOTE(_F5), QD_NOTE(_C5), \
+    HD_NOTE(_A4), QD_NOTE(_F4),  QD_NOTE(_C5), WD_NOTE(_A4)
+
+    #undef RICK_ROLL
+    #define RICK_ROLL      \
+        Q__NOTE(_F4),      \
+        Q__NOTE(_G4),      \
+        Q__NOTE(_BF4),     \
+        Q__NOTE(_G4),      \
+        HD_NOTE(_D5),      \
+        HD_NOTE(_D5),      \
+        W__NOTE(_C5),      \
+        S__NOTE(_REST),    \
+        Q__NOTE(_F4),      \
+        Q__NOTE(_G4),      \
+        Q__NOTE(_BF4),     \
+        Q__NOTE(_G4),      \
+        HD_NOTE(_C5),      \
+        HD_NOTE(_C5),      \
+        W__NOTE(_BF4),     \
+        S__NOTE(_REST),    \
+        Q__NOTE(_F4),      \
+        Q__NOTE(_G4),      \
+        Q__NOTE(_BF4),     \
+        Q__NOTE(_G4),      \
+        W__NOTE(_BF4),     \
+        H__NOTE(_C5),      \
+        H__NOTE(_A4),      \
+        H__NOTE(_A4),      \
+        H__NOTE(_G4),      \
+        H__NOTE(_F4),      \
+        H__NOTE(_F4),      \
+        W__NOTE(_C5),      \
+        W__NOTE(_BF4),
+#endif
